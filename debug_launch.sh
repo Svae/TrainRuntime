29088cfc2356e1e2bac4c47b1faf2befb7030056
Henrik Heggelund Svendsen <hsvendsen@gmail.com>
2016-03-11 14:06:36 +0100

    change launch options\n\nwrite all debug messages to console

--- debug_launch.sh
+++ debug_launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-java -Xdebug -Xrunjdwp:transport=dt_socket,address=8001,server=y,suspend=y -jar `ls -rt | grep org.eclipse.osgi_3.10*.jar | tail -n1` -console
+java -jar `ls -rt | grep org.eclipse.osgi_3.10*.jar | tail -n1` -console -Dequinox.ds.print=true
 
 
